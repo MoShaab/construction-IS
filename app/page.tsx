c2a5211d1154036c2e94fa0c6f2fe76804dd56dc
Shaab <mxassanciiro@gmail.com>
2024-12-04 19:02:12 +0300

    overhauled the project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,3 @@
-
-// import { fetchAllInventory, fetchProjects, fetchSuppliers, fetchUsers } from "@/lib/data";
-// import Dashboard from "@/components/dashboard";
-// export const revalidate = 0;
-
-// export default async function Page() {
-//   const allInventory = await fetchAllInventory();
-//   const projects = await fetchProjects();
-//   const suppliers = await fetchSuppliers();
-//   const users = await fetchUsers();
-
-//   return (
-//     <Dashboard
-//       allInventory={allInventory}
-//       projects={projects}
-//       suppliers={suppliers}
-//       users={users}
-//     />
-//   );
-// }
+export default function Page(){
+  return <p>Customers page</p>;
+}
